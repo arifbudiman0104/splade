36f13f1d1d32fa80ca6d33d5461bfc60e93bb698
arifbudimanar <arifbudiman01041996@hotmail.com>
2023-01-11 19:13:41 +0700

    add banner homepage

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,4 +1,17 @@
 <div class="min-h-screen bg-gray-100">
+    @if (Route::currentRouteName() == 'home' && Auth::check()===false)
+    <div class="hidden text-gray-500 bg-gray-50  md:block">
+        <div class="flex flex-col items-center justify-center max-w-3xl p-3 mx-auto text-sm" role="alert">
+            <p class="text-center">If you're a first-time visitor to my website, I invite you to leave your mark by
+                signing my <Link href="{{ route("guestbook.index") }}"
+                    class="text-orange-400 dark:text-orange-500">guestbook</Link>.
+            <p>I appreciate your feedback and thoughts,
+                and hope that you enjoy your visit to my
+                website.</p>
+            </p>
+        </div>
+    </div>
+    @endif
     @include('layouts.guest-navigation')
 
     <!-- Page Heading -->
