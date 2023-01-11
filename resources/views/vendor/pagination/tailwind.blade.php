5b638c8dda149f0249d844e88076af7aaf64823f
arifbudimanar <arifbudiman01041996@hotmail.com>
2023-01-11 19:50:16 +0700

    fix pagination

--- resources/views/vendor/pagination/tailwind.blade.php
+++ resources/views/vendor/pagination/tailwind.blade.php
@@ -1,6 +1,6 @@
 @if ($paginator->hasPages())
     <nav role="navigation" aria-label="{{ __('Pagination Navigation') }}" class="flex items-center justify-between">
-        <div class="flex justify-between flex-1 sm:hidden">
+        <div class="flex justify-between flex-1 md:hidden">
             @if ($paginator->onFirstPage())
                 <span class="relative inline-flex items-center px-4 py-2 text-sm font-medium leading-5 text-gray-500 bg-gray-100  rounded-md cursor-default">
                     {!! __('pagination.previous') !!}
@@ -22,9 +22,9 @@
             @endif
         </div>
 
-        <div class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
+        <div class="hidden sm:flex-1 md:flex sm:items-center sm:justify-between">
             <div>
-                <p class="text-sm leading-5 text-gray-700">
+                {{-- <p class="text-sm leading-5 text-gray-700">
                     {!! __('Showing') !!}
                     @if ($paginator->firstItem())
                         <span class="font-medium">{{ $paginator->firstItem() }}</span>
@@ -36,7 +36,7 @@
                     {!! __('of') !!}
                     <span class="font-medium">{{ $paginator->total() }}</span>
                     {!! __('results') !!}
-                </p>
+                </p> --}}
             </div>
 
             <div>
